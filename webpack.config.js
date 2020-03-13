6114d61cfcb10797cfe6597d0618c844b973b42c
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2020-03-13 12:33:34 -0400

    Base react app

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundler.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  mode: process.env.NODE_ENV === "production" ? "production" : "development"
+};
