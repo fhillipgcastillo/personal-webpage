7f2ecb2ff0ad429f6057c0ae6ead4484862b752e
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2020-03-14 15:43:49 -0400

    WIP - automated saved

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -130,10 +130,7 @@ export default class Main extends Component {
               alignItems="center"
             >
               <Grid item xs={6} style={{...useStyle.textCenter}}> 
-                An artist of considerable range, Chet Faker — the name taken by
-                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-                and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.
+                {profile.geo}
               </Grid>
             </Grid>
           </Grid>
