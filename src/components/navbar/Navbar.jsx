185ba88f117bf9065075a943e8b689099c773982
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2020-03-15 00:20:10 -0400

    WIP - automated saved

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Navba() {
           </Typography>
         </IconButton>
         <div className={classes.grow3} />
-        <Grid item container className={classes.grow}>
+        <Grid container className={classes.grow}>
           <Typography variant="h6" className={classes.title}>
             <Link to="/portfolio">Portfolio</Link>
           </Typography>
