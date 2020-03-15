a3d5ae82a03e89592857d7d72e5b84e50806208d
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2020-03-15 00:14:17 -0400

    WIP - automated saved

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
-        test: /\.(jpg)$/,
+        test: /\.(png|jpe?g|gif)$/,
         use: "file-loader"
       }
     ]
